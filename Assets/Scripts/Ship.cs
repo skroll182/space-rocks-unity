0d2dfa29c0b2175248b6479c61346791d2aa10b9
Kirill Olar <kirill.olar26@gmail.com>
2019-05-30 09:40:03 +0600

    Initial commit Created controls for ship

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Ship : MonoBehaviour
+{
+    private const int _rotationSpeed = 200;
+    private float _speed;
+    private Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _speed = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody2D.transform.Rotate(new Vector3(0, 0, 1), _rotationSpeed*Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody2D.transform.Rotate(new Vector3(0, 0, 1), -_rotationSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _rigidbody2D.AddRelativeForce(Vector2.right * _speed * Time.deltaTime);
+        }
+    
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Space Rocks(Unity)
+Recreation of "Space Rocks" game shown in [tutorial series](https://www.youtube.com/playlist?list=PLhIbBGhnxj5I8HuwoNhjgjDC8OQIf8D3Z "My First Game - GML Space Rocks") for **Game Maker: Studio 2** on **Unity**.
+
+Required:
+
+- Unity 2019.1
