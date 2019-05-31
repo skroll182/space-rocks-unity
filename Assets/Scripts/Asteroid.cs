10eedbe2a14c895f58b98c2b148f2fa030701e67
Kirill <kirill.olar26@gmail.com>
2019-05-31 09:55:29 +0600

    Added asteroid movement Added asteroid generation Added colliders to ship and asteroids TODO: polish asteroid colliders

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 0.5f;
+    [SerializeField] private float minSpeed = -0.5f;
+    [SerializeField] private float rotationSpeed = 50.0f;
+    [SerializeField] private Sprite[] sprites = new Sprite[3];
+    private Rigidbody2D _rigidbody2D;
+    private Vector2 _moveDirection;
+    private float _upperBound;
+    private float _rightBound;
+    private float _spriteWidth;
+    private float _spriteHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _moveDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        _rigidbody2D.velocity = new Vector2(Random.Range(minSpeed, maxSpeed), Random.Range(minSpeed, maxSpeed));
+        Sprite spr = GetComponent<SpriteRenderer>().sprite;
+        _spriteWidth = spr.texture.width / spr.pixelsPerUnit;
+        _spriteHeight = spr.texture.height / spr.pixelsPerUnit;
+        _upperBound = Camera.main.transform.position.y + Camera.main.orthographicSize;
+        _rightBound = Camera.main.transform.position.x + Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rigidbody2D.transform.position.x >= _rightBound + _spriteWidth / 4)
+        {
+            _rigidbody2D.transform.position = new Vector3(-_rightBound - _spriteWidth / 4, _rigidbody2D.transform.position.y);
+        }
+        if (_rigidbody2D.transform.position.x <= -_rightBound - _spriteWidth / 4)
+        {
+            _rigidbody2D.transform.position = new Vector3(_rightBound + _spriteWidth / 4, _rigidbody2D.transform.position.y);
+        }
+        if (_rigidbody2D.transform.position.y >= _upperBound + _spriteHeight / 4)
+        {
+            _rigidbody2D.transform.position = new Vector3(_rigidbody2D.transform.position.x, -_upperBound - _spriteHeight / 4);
+        }
+        if (_rigidbody2D.transform.position.y <= -_upperBound - _spriteHeight / 4)
+        {
+            _rigidbody2D.transform.position = new Vector3(_rigidbody2D.transform.position.x, _upperBound + _spriteHeight / 4);
+        }
+        _rigidbody2D.transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class GameSceneController : MonoBehaviour
 {
-    [SerializeField] private GameObject ShipPrefab;
-    private Camera _camera;
+    [SerializeField] private GameObject AsteroidPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
-        _camera = Camera.main;
+        Camera cam = Camera.main;
+
+        
+
+        for(int i = 0; i < 20; i++)
+        {
+            float[] X = new float[] {
+                Random.Range(
+                    cam.transform.position.x - cam.orthographicSize * cam.aspect,
+                    cam.transform.position.x - cam.orthographicSize * cam.aspect + cam.orthographicSize * cam.aspect * 2.0f * 0.3f
+                ),
+                Random.Range(
+                    cam.transform.position.x + cam.orthographicSize * cam.aspect - cam.orthographicSize * cam.aspect * 2.0f * 0.3f,
+                    cam.transform.position.x + cam.orthographicSize * cam.aspect
+                )
+            };
+
+            float[] Y = new float[] {
+                Random.Range(
+                    cam.transform.position.y - cam.orthographicSize,
+                    cam.transform.position.y - cam.orthographicSize + cam.orthographicSize * 2.0f * 0.3f
+                ),
+                Random.Range(
+                    cam.transform.position.y + cam.orthographicSize - cam.orthographicSize * 2.0f * 0.3f,
+                    cam.transform.position.y + cam.orthographicSize
+                )
+            };
+
+            float x = X[Random.Range(0, X.Length)];
+            float y = Y[Random.Range(0, Y.Length)];
+            float z = 1.0f;
+
+            Vector3 pos = new Vector3(x, y, z);
+            Instantiate(AsteroidPrefab, pos, new Quaternion(0,0,0,0));
+        }
     }
 
     // Update is called once per frame
